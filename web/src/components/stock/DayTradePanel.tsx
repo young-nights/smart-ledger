d448d9b095d55dc746e3683caf7db4f762b0ddb2
young-nights <2863692760@qq.com>
2026-06-29 17:03:24 +0800

    fix: shift fee header right 8px more (marginLeft: 24)

--- web/src/components/stock/DayTradePanel.tsx
+++ web/src/components/stock/DayTradePanel.tsx
@@ -1134,7 +1134,7 @@ export function DayTradePanel({
                       <span style={{ flex: 1, minWidth: 0, textAlign: "center" }}>卖出金额</span>
                       <span style={{ flex: 1, minWidth: 0, textAlign: "center" }}>盈亏</span>
                       <span style={{ flex: 1, minWidth: 0, textAlign: "center", marginLeft: 32 }}>数量</span>
-                      <span style={{ flex: 1, minWidth: 0, textAlign: "center", marginLeft: 16, marginRight: 30 }}>手续费</span>
+                      <span style={{ flex: 1, minWidth: 0, textAlign: "center", marginLeft: 24, marginRight: 30 }}>手续费</span>
                       <span style={{ width: 42, flexShrink: 0 }}>{""}</span>{/* action buttons */}
                     </div>
                     {group.matches.map((m, idx) => {
